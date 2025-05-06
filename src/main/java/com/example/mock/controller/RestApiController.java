b001ab86a75b03de0f0c47164e06897f429aacaa
darknesszxc <antoniuk.mishany@yandex.ru>
2025-05-06 18:40:03 +0300

    Fix

--- src/main/java/com/example/mock/controller/RestApiController.java
+++ src/main/java/com/example/mock/controller/RestApiController.java
@@ -1,4 +1,34 @@
 package com.example.mock.controller;
 
+import com.example.mock.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("/api")
 public class RestApiController {
+
+    private void sleepRandomTime() {
+        try {
+            TimeUnit.SECONDS.sleep((long)(Math.random() * 2 + 1));
+        } catch (InterruptedException e) {
+            System.err.println("Sleep interrupted: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/getUserInfo")
+    public String getUserInfo() {
+        sleepRandomTime();
+        return "{\"login\":\"Login1\",\"status\":\"ok\"}";
+    }
+
+    @PostMapping(value = "/authenticate", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<User> authenticate(@RequestBody User inputUser) {
+        sleepRandomTime();
+        User userWithDate = new User(inputUser.getLogin(), inputUser.getPassword());
+        System.out.println("Returning user: " + userWithDate);
+        return ResponseEntity.ok(userWithDate);
+    }
 }
--- src/main/java/com/example/mock/model/User.java
+++ src/main/java/com/example/mock/model/User.java
@@ -1,4 +1,24 @@
 package com.example.mock.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class User {
+    private String login;
+    private String password;
+    private String date;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+        this.date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
 }
